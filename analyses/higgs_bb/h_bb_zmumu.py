f10cbfb9c06cf58ad4fb418c746f1a58d4051593
Caitlin Kubina <137904203+ckubina@users.noreply.github.com>
2024-03-09 18:13:04 -0500

    Update h_bb_zmumu.py--Change b quark probability cut to 0.95

--- analyses/higgs_bb/h_bb_zmumu.py
+++ analyses/higgs_bb/h_bb_zmumu.py
@@ -288,7 +288,7 @@ def build_graph(df, dataset):
     results.append(df.Histo1D(("recojet_isG_jet0", "", *bins_score), "recojet_isG_jet0"))
     results.append(df.Histo1D(("recojet_isG_jet1", "", *bins_score), "recojet_isG_jet1"))
 
-    df = df.Filter("recojet_isB_jet0 > 0.97 && recojet_isB_jet1 >0.97")
+    df = df.Filter("recojet_isB_jet0 > 0.95 && recojet_isB_jet1 >0.95")
     results.append(df.Histo1D(("cutFlow", "", *bins_count), "cut9"))
 
     #Reconstruct the Higgs Mass
