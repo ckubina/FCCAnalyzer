b83e20f5cf72e57222359bb6827d80590bb5c691
Caitlin Kubina <137904203+ckubina@users.noreply.github.com>
2024-03-10 16:34:30 -0400

    Update h_bb_zee.py--- uncomment jet-truth analysis

--- analyses/higgs_bb/h_bb_zee.py
+++ analyses/higgs_bb/h_bb_zee.py
@@ -294,13 +294,13 @@ def build_graph(df, dataset):
     results.append(df.Histo1D(("dijet_higgs_m_reco", "", *bins_m), "dijet_higgs_m_reco"))
 
     # compare with jet-truth analysis
-    # df = df.Define("jets_mc", "FCCAnalyses::jetTruthFinder(_jetc, rps_no_electrons, Particle, MCRecoAssociations1)")
-    # df = df.Define("njets", f"{njets}")
-    # df = df.Define("jets_higgs_mc", "FCCAnalyses::Vec_i res; for(int i=0;i<njets;i++) if(abs(jets_mc[i])==5) res.push_back(i); return res;") # assume H->bb
-    # df = df.Filter("jets_higgs_mc.size()==2")
-    # df = df.Define("dijet_higgs_m_mc", "(jet_tlv[jets_higgs_mc[0]]+jet_tlv[jets_higgs_mc[1]]).M()")
+    df = df.Define("jets_mc", "FCCAnalyses::jetTruthFinder(_jetc, rps_no_electrons, Particle, MCRecoAssociations1)")
+    df = df.Define("njets", f"{njets}")
+    df = df.Define("jets_higgs_mc", "FCCAnalyses::Vec_i res; for(int i=0;i<njets;i++) if(abs(jets_mc[i])==5) res.push_back(i); return res;") # assume H->bb
+    df = df.Filter("jets_higgs_mc.size()==2")
+    df = df.Define("dijet_higgs_m_mc", "(jet_tlv[jets_higgs_mc[0]]+jet_tlv[jets_higgs_mc[1]]).M()")
 
-    # results.append(df.Histo1D(("dijet_higgs_m_mc", "", *bins_m), "dijet_higgs_m_mc")) #monte carlo higgs mass
+    results.append(df.Histo1D(("dijet_higgs_m_mc", "", *bins_m), "dijet_higgs_m_mc")) #monte carlo higgs mass
 
     return results, weightsum
 
